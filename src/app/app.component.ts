50cf63ca59d7774babcad2ba18e0840d823eeea0
duynv1895 <duynv@ql6625.fr>
2020-09-19 18:03:17 +0700

    Upload image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+  title = 'Upload file';
+  images: File;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  selectImage(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.images = file;
+      console.log(this.images);
+    }
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.images);
+
+    this.http.post<any>('http://localhost:3000/file', formData).subscribe(
+      (data) => console.log(data),
+      (err) => console.log(err)
+    );
+  }
 }
